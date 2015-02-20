e23d17224c03305c400870deadfd45f06668cec2
jim-clark <email@jim-clark.com>
2015-02-20 13:32:39 -0800

    Gulp now spins up mongo

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@
       nodemon = require('gulp-nodemon'),
       uglify = require('gulp-uglify'),
       concat = require('gulp-concat'),
-      templateCache = require('gulp-angular-templatecache');
+      templateCache = require('gulp-angular-templatecache'),
+      child_process = require('child_process');
 
 
   // builds an anglular module, 'templates', as 'templates.js'
@@ -21,8 +22,29 @@
           .pipe(gulp.dest('src/js'));
   });
 
+
+  /*  DEVELOPMENT TASKS FOR 'default' */
+
+  // var cmdScript = "osascript -e tell application 'iTerm2'\n activate \n set db to (make new terminal) \n exec command 'mongod'\n end tell";
+  // var cmdScript = 'osascript -e tell application "System Events" to tell process "iTerm2" to keystroke "t" using {command down} end tell';
+
+  // Start mongodb
+  gulp.task('start-mongodb', function () {
+    child_process.exec('mongod', function (err, stdout, stderr) {
+      if (err) {console.log(err)}
+      console.log(stdout);
+    });
+  });
+
+  // copy less and css stylesheets to public/css
+  // less files are compiled upon browser request by express middleware
+  gulp.task('copy-stylesheets', function () {
+    gulp.src('./src/stylesheets/*.*')
+      .pipe(gulp.dest('./public/css'));
+  });
+  
   // register nodemon task
-  gulp.task('nodemon', function() {
+  gulp.task('nodemon', ['start-mongodb'], function () {
     nodemon({
       script: './server.js',
       ext: 'js html',
@@ -31,8 +53,16 @@
       .on('restart');
   });
 
+  /* END DEVELOPMENT TASKS */
+
+
   // runs when you run 'gulp'
-  gulp.task('default', ['angular-templates', 'nodemon']);
+  gulp.task('default', [
+    'start-mongodb',
+    'angular-templates', 
+    'copy-stylesheets',
+    'nodemon'
+  ]);
 
 
 })();
--- server.js
+++ server.js
@@ -75,16 +75,16 @@ app.use(function(err, req, res, next) {
 // mongoose.connect('mongodb://localhost/passport_local_mongoose');
 
 var db_name = 'carf';
-//provide a sensible default for local development
-mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
-//take advantage of openshift env vars when available:
-if(process.env.OPENSHIFT_MONGODB_DB_URL){
-  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
-}
+mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL ?
+  process.env.OPENSHIFT_MONGODB_DB_URL + db_name : 'mongodb://127.0.0.1:27017/' + db_name;
+
+console.log(mongodb_connection_string)
+
+mongoose.connect(mongodb_connection_string);
+
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
 var server = app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ":" + server_port )
 });
