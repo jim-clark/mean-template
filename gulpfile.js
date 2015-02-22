fa09b55867cbabcc8f3218366878a5b6c1272559
jim-clark <email@jim-clark.com>
2015-02-22 07:30:50 -0800

    Added 'test' task in gulpfile.js to auto rerun jasmine tests when specs are updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,80 @@
 (function() {
 
-  // grab our gulp packages
-  var gulp = require('gulp'),
-      gutil = require('gulp-util'),
-      nodemon = require('gulp-nodemon'),
-      uglify = require('gulp-uglify'),
-      concat = require('gulp-concat'),
-      templateCache = require('gulp-angular-templatecache'),
-      child_process = require('child_process');
+    // grab our gulp packages
+    var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    nodemon = require('gulp-nodemon'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    templateCache = require('gulp-angular-templatecache'),
+    child_process = require('child_process'),
+    shell = require('gulp-shell');
 
 
-  // builds an anglular module, 'templates', as 'templates.js'
-  gulp.task('angular-templates', function () {
-      gulp.src('./src/**/*.html')
-          .pipe(templateCache(
+    /*
+        TESTING TASKS
+        */
+
+        gulp.task('jasmine', shell.task(['jasmine']));
+
+        gulp.task('watchTest', function () {
+            gulp.watch(['./spec/**/*spec.js'], ['jasmine']);
+        });
+
+        gulp.task('test', ['jasmine', 'watchTest']);
+
+
+    /*
+        DEVELOPMENT TASKS FOR 'default'
+        */
+
+    // builds an anglular module, 'templates', as 'templates.js'
+    gulp.task('angular-templates', function () {
+        gulp.src('./src/**/*.html')
+        .pipe(templateCache(
             'templates.js',
             {
-              standalone: true,
-              module: 'templates'
+                standalone: true,
+                module: 'templates'
             }))
-          .pipe(gulp.dest('src/js'));
-  });
-
-
-  /*  DEVELOPMENT TASKS FOR 'default' */
-
-  // var cmdScript = "osascript -e tell application 'iTerm2'\n activate \n set db to (make new terminal) \n exec command 'mongod'\n end tell";
-  // var cmdScript = 'osascript -e tell application "System Events" to tell process "iTerm2" to keystroke "t" using {command down} end tell';
-
-  // Start mongodb
-  gulp.task('start-mongodb', function () {
-    child_process.exec('mongod', function (err, stdout, stderr) {
-      if (err) {console.log(err)}
-      console.log(stdout);
+        .pipe(gulp.dest('src/js'));
     });
-  });
 
-  // copy less and css stylesheets to public/css
-  // less files are compiled upon browser request by express middleware
-  gulp.task('copy-stylesheets', function () {
-    gulp.src('./src/stylesheets/*.*')
-      .pipe(gulp.dest('./public/css'));
-  });
-  
-  // register nodemon task
-  gulp.task('nodemon', ['start-mongodb'], function () {
-    nodemon({
-      script: './server.js',
-      ext: 'js html',
-      env: { 'NODE_ENV': 'development' }
-    })
-      .on('restart');
-  });
+    // Start mongodb
+    gulp.task('start-mongodb', function () {
+        child_process.exec('mongod', function (err, stdout, stderr) {
+            if (err) { console.log(err); }
+            console.log(stdout);
+        });
+    });
 
-  /* END DEVELOPMENT TASKS */
+    // copy less and css stylesheets to public/css
+    // less files are compiled upon browser request by express middleware
+    gulp.task('copy-stylesheets', function () {
+        gulp.src('./src/stylesheets/*.*')
+        .pipe(gulp.dest('./public/css'));
+    });
+
+    // register nodemon task
+    gulp.task('nodemon', ['start-mongodb'], function () {
+        nodemon({
+            script: './server.js',
+            ext: 'js html',
+            env: { 'NODE_ENV': 'development' }
+        })
+        .on('restart');
+    });
+
+    /* END DEVELOPMENT TASKS */
 
 
-  // runs when you run 'gulp'
-  gulp.task('default', [
-    'start-mongodb',
-    'angular-templates', 
-    'copy-stylesheets',
-    'nodemon'
-  ]);
+    // runs when you run 'gulp'
+    gulp.task('default', [
+        'start-mongodb',
+        'angular-templates', 
+        'copy-stylesheets',
+        'nodemon'
+        ]);
 
 
 })();
