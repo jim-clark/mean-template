b9734f53ca35fa9a18f59c9a893c2fcfa802b00f
jim-clark <email@jim-clark.com>
2015-02-22 15:35:15 -0800

    Modified server.js and database.js to ease testing

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
 
 var app = express();
 require('./server/routes/index')(app);
+app.mongoose = mongoose;  // expose for testing
 
 // view engine setup
 app.set('views', path.join(__dirname, 'server', 'views'));
@@ -22,6 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+// use gulp to compile less instead
 // app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
@@ -29,9 +31,6 @@ app.use(cors());
 // setup passport
 // app.use(passport.initialize());
 // app.use(passport.session);
-
-//  TESTING /////////////
-var User = require('./server/models/user');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -74,10 +73,14 @@ app.use(function(err, req, res, next) {
 // db will be mongoose connected according to the op environment
 var db = require('./server/config/database');
 
+// provide a callback for tests
+app.ready = null;
+
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var server = app.listen(server_port, server_ip_address, function () {
+app.server = app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ":" + server_port );
+  if (app.ready) { app.ready(); }
 });
 
 
--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,17 @@
 var mongoose = require('mongoose');
 var constants = require('./constants');
-var mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL ?
-  process.env.OPENSHIFT_MONGODB_DB_URL + constants.DB_NAME : 'mongodb://127.0.0.1:27017/' + constants.DB_NAME;
+var mongodb_connection_string;
+
+switch (true) {
+    case process.env.OPENSHIFT_MONGODB_DB_URL:
+        mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + constants.DB_NAME;
+        break;
+    case process.env.NODE_ENV === 'development':
+        mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + constants.DB_NAME;
+        break;
+    case process.env.NODE_ENV === 'test':
+        mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + constants.TEST_DB_NAME;
+}
 
 mongoose.connect(mongodb_connection_string);
 
